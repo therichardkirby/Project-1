fa9706a6dca4a8ced94bae25b3dc009047ce86db
Richard Kirby <richardkirby89@gmail.com>
2016-02-23 13:31:52 -0500

    Add Project1.php file

---
+++ Project1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+      text-align: center;
+}
+h1   {color: blue;}
+h2    {color: green;}
+</style>
+</head>
+<body>
+
+<h1>Richard Kirby</h1>
+<img src=“https://lh5.googleusercontent.com/-3_sR4b_pCl8/AAAAAAAAAAI/AAAAAAAAAA4/9IfsSttqjSY/photo.jpg” alt=“Kirby”;>
+
+<h2>Project 2</h2>
+<p>View:[link]<p>
+<p>Github: [link]<p>
+<h2>Project 3</h2>
+<p>View: [link]<p>
+<p>Github: [link]<p>
+<h2>Project 4</h2>
+<p>View: [link]<p>
+<p>Github: [link]<p>
+
+</body>
+</html>
